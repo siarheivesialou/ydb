e86927595d262ac33df8abee6adf775f144f0934
Sergey Veselov <sergeyveselov@ydb.tech>
2024-04-10 14:39:18 +0000

    add ydb_ymq client

---
+++ ydb/public/sdk/cpp/client/ydb_ymq/ymq.cpp
@@ -0,0 +1,103 @@
+#include "ymq.h"
+
+#define INCLUDE_YDB_INTERNAL_H
+#include <ydb/public/sdk/cpp/client/impl/ydb_internal/make_request/make.h>
+#undef INCLUDE_YDB_INTERNAL_H
+
+#include <ydb/library/yql/public/issue/yql_issue.h>
+#include <ydb/library/yql/public/issue/yql_issue_message.h>
+
+#include <ydb/public/sdk/cpp/client/ydb_common_client/impl/client.h>
+
+namespace NYdb::NDataStreams::V1 {
+
+    class TYmqClient::TImpl : public TClientImplCommon<TYmqClient::TImpl> {
+    public:
+        TImpl(std::shared_ptr <TGRpcConnectionsImpl> &&connections, const TCommonClientSettings &settings)
+                : TClientImplCommon(std::move(connections), settings) {}
+
+        template<class TProtoResult, class TResultWrapper>
+        auto MakeResultExtractor(NThreading::TPromise <TResultWrapper> promise) {
+            return [promise = std::move(promise)]
+                    (google::protobuf::Any *any, TPlainStatus status) mutable {
+                std::unique_ptr <TProtoResult> result;
+                if (any) {
+                    result.reset(new TProtoResult);
+                    any->UnpackTo(result.get());
+                }
+
+                promise.SetValue(
+                        TResultWrapper(
+                                TStatus(std::move(status)),
+                                std::move(result)));
+            };
+        }
+
+        template<class TProtoService, class TProtoRequest, class TProtoResponse, class TProtoResult, class TSettings, class TFillRequestFn, class TAsyncCall>
+        NThreading::TFuture<TProtoResultWrapper<TProtoResult>> CallImpl(const TSettings& settings, TAsyncCall grpcCall, TFillRequestFn fillRequest) {
+            using TResultWrapper = TProtoResultWrapper<TProtoResult>;
+            auto request = MakeOperationRequest<TProtoRequest>(settings);
+            fillRequest(request);
+
+            auto promise = NThreading::NewPromise<TResultWrapper>();
+            auto future = promise.GetFuture();
+
+            auto extractor = MakeResultExtractor<TProtoResult, TResultWrapper>(std::move(promise));
+
+            Connections_->RunDeferred<TProtoService, TProtoRequest, TProtoResponse>(
+                    std::move(request),
+                    std::move(extractor),
+                    grpcCall,
+                    DbDriverState_,
+                    INITIAL_DEFERRED_CALL_DELAY,
+                    TRpcRequestSettings::Make(settings));
+
+            return future;
+
+        }
+
+        template<class TProtoService, class TProtoRequest, class TProtoResponse, class TProtoResult, class TSettings, class TAsyncCall>
+        NThreading::TFuture<TProtoResultWrapper<TProtoResult>> CallImpl(const TSettings& settings, TAsyncCall grpcCall) {
+            return CallImpl<TProtoService, TProtoRequest, TProtoResponse, TProtoResult>(settings, grpcCall, [](TProtoRequest&) {});
+        }
+
+        TAsyncGetQueueUrlResult GetQueueUrl(const TString &queueName, TGetQueueUrlSettings settings) {
+            return CallImpl<Ydb::Ymq::V1::YmqService,
+                    Ydb::Ymq::V1::GetQueueUrlRequest,
+                    Ydb::Ymq::V1::GetQueueUrlResponse,
+                    Ydb::Ymq::V1::GetQueueUrlResult>(settings,
+                        &Ydb::Ymq::V1::YmqService::Stub::AsyncGetQueueUrl,
+                        [&](Ydb::Ymq::V1::GetQueueUrlRequest& req) {
+                            req.set_queue_name(queueName);
+                        }
+            );
+
+        }
+        
+        template<class TProtoRequest, class TProtoResponse, class TProtoResult, class TMethod>
+        NThreading::TFuture<TProtoResultWrapper<TProtoResult>> DoProtoRequest(const TProtoRequest& proto, TMethod method, const TProtoRequestSettings& settings) {
+            return CallImpl<Ydb::Ymq::V1::YmqService, TProtoRequest, TProtoResponse, TProtoResult>(settings, method,
+               [&](TProtoRequest& req) {
+                    req.CopyFrom(proto);
+               });
+        }
+    };
+
+    template<class TProtoRequest, class TProtoResponse, class TProtoResult, class TMethod>
+    NThreading::TFuture<TProtoResultWrapper<TProtoResult>> TYmqClient::DoProtoRequest(const TProtoRequest& request, TMethod method, TProtoRequestSettings settings) {
+        return Impl_->DoProtoRequest<TProtoRequest, TProtoResponse, TProtoResult, TMethod>(request, method, settings);
+    }
+
+    template NThreading::TFuture<TProtoResultWrapper<Ydb::Ymq::V1::GetQueueUrlResult>> TYmqClient::DoProtoRequest
+                <
+                    Ydb::Ymq::V1::GetQueueUrlRequest,
+                    Ydb::Ymq::V1::GetQueueUrlResponse,
+                    Ydb::Ymq::V1::GetQueueUrlResult,
+                    decltype(&Ydb::Ymq::V1::YmqService::Stub::AsyncGetQueueUrl)
+                >(
+                        const Ydb::Ymq::V1::GetQueueUrlRequest& request,
+                        decltype(&Ydb::Ymq::V1::YmqService::Stub::AsyncGetQueueUrl) method,
+                        TProtoRequestSettings settings
+                );
+}
+
---
+++ ydb/public/sdk/cpp/client/ydb_ymq/ymq.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <ydb/public/sdk/cpp/client/ydb_table/table.h>
+
+#include <ydb/public/api/grpc/draft/ydb_ymq_v1.grpc.pb.h>
+
+namespace NYdb::NDataStreams::V1 {
+
+    template<class TProtoResult>
+    class TProtoResultWrapper : public NYdb::TStatus {
+        friend class TYmqClient;
+
+    private:
+        TProtoResultWrapper(
+                NYdb::TStatus&& status,
+                std::unique_ptr<TProtoResult> result)
+                : TStatus(std::move(status))
+                , Result(std::move(result))
+        { }
+
+    public:
+        const TProtoResult& GetResult() const {
+            Y_ABORT_UNLESS(Result, "Uninitialized result");
+            return *Result;
+        }
+
+    private:
+        std::unique_ptr<TProtoResult> Result;
+    };
+
+    enum EStreamMode {
+        ESM_PROVISIONED = 1,
+        ESM_ON_DEMAND = 2,
+    };
+
+    using TGetQueueUrlResult = TProtoResultWrapper<Ydb::Ymq::V1::GetQueueUrlResult>;
+
+    using TAsyncGetQueueUrlResult = NThreading::TFuture<TGetQueueUrlResult>;
+
+    struct TDataRecord {
+        TString Data;
+        TString PartitionKey;
+        TString ExplicitHashDecimal;
+    };
+
+    struct TGetQueueUrlSettings : public NYdb::TOperationRequestSettings<TGetQueueUrlSettings> {};
+    struct TProtoRequestSettings : public NYdb::TOperationRequestSettings<TProtoRequestSettings> {};
+
+    class TYmqClient {
+        class TImpl;
+
+    public:
+        TYmqClient(const NYdb::TDriver& driver, const NYdb::TCommonClientSettings& settings = NYdb::TCommonClientSettings());
+
+        TAsyncGetQueueUrlResult GetQueueUrl(const TString& queueName, TGetQueueUrlSettings& createStreamSettings);
+
+        template<class TProtoRequest, class TProtoResponse, class TProtoResult, class TMethod>
+        NThreading::TFuture<TProtoResultWrapper<TProtoResult>> DoProtoRequest(const TProtoRequest& request, TMethod method, TProtoRequestSettings settings = TProtoRequestSettings());
+
+        NThreading::TFuture<void> DiscoveryCompleted();
+
+    private:
+        std::shared_ptr<TImpl> Impl_;
+    };
+
+}
